4536ccbb774606141f953dc48eb78fe44cfeb750
Michael Henretty <michael.henretty@gmail.com>
2017-04-11 20:40:35 +0200

    move clip handling stuff out of the main server file

---
+++ server/lib/clip.js
@@ -0,0 +1,85 @@
+(function() {
+  'use strict';
+
+  const glob = require('glob');
+  const ms = require('mediaserver');
+  const path = require('path');
+  const ff = require('ff');
+  const fs = require('fs');
+
+  const UPLOAD_PATH = path.resolve(__dirname, '..', 'upload', 'recorded');
+
+  const ACCEPTED_EXT = [ 'ogg', 'webm', 'm4a' ];
+
+  module.exports = {
+
+    save: function(request) {
+      let info = request.headers;
+      let uid = info.uid;
+      let sentence = info.sentence;
+      let assignmentId = info.assignmentid;
+
+      return new Promise((resolve, reject) => {
+        let extension = '.ogg';  // Firefox gives us opus in ogg
+        if (info['content-type'].startsWith('audio/webm')) {
+          extension = '.webm';   // Chrome gives us opus in webm
+        } else if (info['content-type'].startsWith('audio/mp4a')) {
+          extension = '.m4a'; // iOS gives us mp4a
+        }
+
+        // if the folder does not exist, we create it
+        let folder = path.join(UPLOAD_PATH, uid);
+        let file = path.join(folder, assignmentId + extension);
+
+        let f = ff(() => {
+          fs.exists(folder, f.slotPlain());
+        }, exists => {
+          if (!exists) {
+            fs.mkdir(folder, f());
+          }
+        }, () => {
+          let writeStream = fs.createWriteStream(file);
+          request.pipe(writeStream);
+          request.on('end', f());
+          fs.writeFile(path.join(folder, assignmentId + '.txt'), sentence, f());
+        }, () => {
+          console.log('file written', file);
+          resolve(assignmentId);
+        }).onError(reject);
+      });
+    },
+
+    serve: function(request, response) {
+      let ids = request.url.split('/');
+      let assignmentId = ids.pop();
+      let workerid = ids.pop();
+      let prefix = path.join(UPLOAD_PATH, workerid, assignmentId);
+
+      glob(prefix + '.*', (err, files) => {
+        if (err) {
+          console.error('could not glob for clip', err);
+          reject(err);
+          return;
+        }
+
+        // Try to find the right file, since we don't know the extension.
+        let file = null;
+        for (let i = 0; i < files.length; i++) {
+          let ext = files[i].split('.').pop();
+          if (ACCEPTED_EXT.indexOf(ext) !== -1) {
+            file = files[i];
+            break;
+          }
+        }
+
+        if (!file) {
+          console.error('could not file clip', files);
+          reject('no clip');
+          return;
+        }
+
+        ms.pipe(request, response, file);
+      });
+    }
+  };
+})();
--- server/server.js
+++ server/server.js
@@ -4,68 +4,21 @@
   const path = require('path');
   const nodeStatic = require('node-static');
   const jsonfile = require('jsonfile');
-  const fs = require('fs');
-  const ff = require('ff');
-  const ms = require('mediaserver');
   const metrics = require('./lib/metrics');
+  const clip = require('./lib/clip');
 
   const DEFAULT_PORT = 9000;
   const CONFIG_FILE = path.resolve(__dirname, '..', 'config.json');
-  const UPLOAD_PATH = path.resolve(__dirname, 'upload', 'recorded');
 
   let fileServer = new nodeStatic.Server('./pub', { cache: false });
 
-  function saveClip(request) {
-    let info = request.headers;
-    let uid = info.uid;
-    let sentence = info.sentence;
-    let assignmentId = info.assignmentid;
-
-    return new Promise((resolve, reject) => {
-      let extension = '.ogg';  // Firefox gives us opus in ogg
-      if (info['content-type'].startsWith('audio/webm')) {
-        extension = '.webm';   // Chrome gives us opus in webm
-      } else if (info['content-type'].startsWith('audio/mp4a')) {
-        extension = '.m4a'; // iOS gives us mp4a
-      }
-
-      // if the folder does not exist, we create it
-      let folder = path.join(UPLOAD_PATH, uid);
-      let file = path.join(folder, assignmentId + extension);
-
-      let f = ff(() => {
-        fs.exists(folder, f.slotPlain());
-      }, exists => {
-        if (!exists) {
-          fs.mkdir(folder, f());
-        }
-      }, () => {
-        let writeStream = fs.createWriteStream(file);
-        request.pipe(writeStream);
-        request.on('end', f());
-        fs.writeFile(path.join(folder, assignmentId + '.txt'), sentence, f());
-      }, () => {
-        console.log('file written', file);
-        resolve(assignmentId);
-      }).onError(reject);
-    });
-  }
-
-  function sendFile(request, response) {
-    let ids = request.url.split('/');
-    let assignmentId = ids.pop();
-    let workerid = ids.pop();
-
-    let filepath = path.resolve(UPLOAD_PATH, workerid, assignmentId + '.ogg');
-    ms.pipe(request, response, filepath);
-  }
 
   jsonfile.readFile(CONFIG_FILE, function(err, config) {
     let port = config.port || DEFAULT_PORT;
     require('http').createServer(function (request, response) {
       if (request.url.includes('/upload/')) {
         if (request.method === 'POST') {
-          saveClip(request).then(timestamp => {
+          clip.save(request).then(timestamp => {
             response.writeHead(200);
             response.end('' + timestamp);
           }).catch(e => {
@@ -74,7 +27,7 @@
             response.end('Error');
           });
         } else {
-          sendFile(request, response);
+          clip.serve(request, response);
         }
 
         return;
