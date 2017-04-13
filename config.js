804af562d3ba3babd1a7d7adba96b93c43df818d
Michael Henretty <michael.henretty@gmail.com>
2017-04-13 18:33:38 +0200

    fix linting errors with new config script

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
-let cache = null;
-module.exports = function(cb) {
-  if (cache) {
-    cb(cache);
-  }
-  let jsonfile = require('jsonfile');
-  jsonfile.readFile(__dirname + '/config.json', (err, config) => {
-    if (err) {
-      console.error('error loading config.json', err);
-      cb({});
-      return;
-    }
+(function() {
+  'use strict';
 
-    cache = config;
-    cb(cache);
-  });
-};
+  let cache = null;
+  module.exports = function(cb) {
+    if (cache) {
+      cb(cache);
+    }
+    let jsonfile = require('jsonfile');
+    jsonfile.readFile(__dirname + '/config.json', (err, config) => {
+      if (err) {
+        console.error('error loading config.json', err);
+        cb({});
+        return;
+      }
+
+      cache = config;
+      cb(cache);
+    });
+  };
+})();
