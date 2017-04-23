e793fc628862fba0735ed5bc5472ef7fb41fa74d
Michael Henretty <michael.henretty@gmail.com>
2017-04-23 19:30:59 +0200

    add defaults to the config.js file

--- config.js
+++ config.js
@@ -1,6 +1,20 @@
 (function() {
   'use strict';
 
+  const DEFAULTS = {
+    "accessKeyId": "",     // AWS creds.
+    "secretAccessKey": "", // AWS creds.
+    "region": "us-east-1",
+    "uploadHost": "", // Server where site lives.
+    "uploadDest": "/home/mikehenrty/mech-turk/",
+    "DB_USER": "", // Mongo instance.
+    "DB_PASS": "",
+    "DB_NAME": "",
+    "DB_PORT": 0,
+    "PROD": false  // Run HITs on production or sandbox.
+  };
+
+
   let cache = null;
   module.exports = function(cb) {
     if (cache) {
@@ -14,7 +28,7 @@
         return;
       }
 
-      cache = config;
+      cache = Object.assign({}, DEFAULTS, config);
       cb(cache);
     });
   };
